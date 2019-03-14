91152a520168d9ebbeed3e6c36a8084f4eab7993
Mustafa Dane <mustafadane@gmail.com>
2019-03-13 23:12:17 -0500

    able to draw 5 cards

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ const axios = require("axios");
 
 console.log('Welcome to my Poker App || Mustafa Dane')
 
-let deckId;
 
 async function shuffleCard () {
     try {
         //axios returs the whole response and the response is grabbed.
         const { data } = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-        console.log(data)
 
-        //if shuffling is success, data object will have success property as true.
+        //if shuffling is success, data object will have success property as true. Function returns deck_id.
         if(data.success) {
-            deckId = data.deck_id
+            return data.deck_id
         } else {
             throw Error('shuffling was not successful!')
         }
     } catch (err) {
-        console.log("Some error happened:", err)
+        console.log("Some error happened during shuffling:", err)
     }
 }
 
+async function draw (deckId) {
+    try {
+        const { data } = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=5`)
+        console.log(data)
+    } catch (err) {
+        console.log("Some error happened during drawing:", err.message)
+    }
+
+}
+
+
 //Everything that app does wrapped in this function to be able to use await.
 async function initializeApp () {
-    await shuffleCard()
+    const deckId = await shuffleCard()
     console.log(deckId)
+    await draw(deckId)
 }
 
 //Then, app is started by calling this function.
